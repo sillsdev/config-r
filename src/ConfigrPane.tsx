d04b67eea0a792819991fc3ffb7ef781fad0376b
John Hatton <hattonjohn@gmail.com>
2022-01-03 10:58:07 -0700

    Extract FilterForSearch

--- src/ConfigrPane.tsx
+++ src/ConfigrPane.tsx
@@ -8,7 +8,7 @@ import { createTheme, Theme, ThemeProvider } from '@mui/material/styles';
 import { useTheme } from '@mui/styles';
 
 import toPath from 'lodash/toPath';
-import Children from 'react-children-utilities';
+
 import { Field, Formik, useField, useFormikContext } from 'formik';
 import {
   Tab,
@@ -32,6 +32,7 @@ import {
 } from '@mui/material';
 import { TextField, Switch, Checkbox, Select as FormikMuiSelect } from 'formik-mui';
 import { HighlightSearchTerms } from './HighlightSearchTerms';
+import { FilterForSearchText, SearchContext } from './FilterForSearch';
 
 type valueGetter = () => Object;
 
@@ -53,11 +54,6 @@ const secondaryGrey = 'rgba(0, 0, 0, 0.54)';
 const FocusPageContext = React.createContext({
   focussedSubPagePath: '',
   setFocussedSubPagePath: (p: string) => {},
-});
-const SearchContext = React.createContext({
-  searchString: '' as string | null,
-  searchRegEx: null as RegExp | null,
-  setSearchString: (searchString: string | null) => {},
 });
 
 export const defaultConfigrTheme = {
@@ -397,57 +393,6 @@ const FilterForSubPage: React.FunctionComponent<{
         return <React.Fragment>{props.children}</React.Fragment>;
       }}
     </FocusPageContext.Consumer>
-  );
-};
-
-// If Search is empty, pass through.
-// Else, pass through so long as the given label or kids have the search term
-const FilterForSearchText: React.FunctionComponent<{
-  label: string;
-  description?: string | React.ReactNode;
-  kids: React.ReactNode;
-}> = (props) => {
-  // enhance: would the "react-children-utilities/Children" utilities simplify this?
-  return (
-    <SearchContext.Consumer>
-      {({ searchRegEx }) => {
-        if (!searchRegEx) return <React.Fragment>{props.children}</React.Fragment>;
-        const hasMatch =
-          /* TODO: currently, if we match on a group's label or description,
-           but then don't match on a subgroup,
-          well we just get the label and that's confusing. */
-          searchRegEx.test(props.label) ||
-          (props.description && searchRegEx.test(Children.onlyText(props.description))) ||
-          // check the children (rows of the group)
-          React.Children.toArray(props.kids).some((c) => {
-            const componentWithLabelProp = (c as any).props?.label;
-            if (componentWithLabelProp && searchRegEx.test(componentWithLabelProp))
-              // this grandchild has the search string, directly
-              return true;
-            const componentWithSearchTermsProp = (c as any).props?.searchTerms;
-            if (
-              componentWithSearchTermsProp &&
-              searchRegEx.test(componentWithSearchTermsProp)
-            )
-              return true;
-            // Is this child a subpage? Then it has children we can check.
-            const childrenOfSubPage = (c as any).props?.children;
-            if (childrenOfSubPage) {
-              // among these children...
-              return React.Children.toArray(childrenOfSubPage).some((c: any) => {
-                // ... is there a child (a row of a sub page) that has a label that matches?
-                if (c?.props?.label) {
-                  // check the label in a great-grand-child (row of a sub page)
-                  return searchRegEx.test(c.props.label);
-                }
-              });
-            }
-          });
-        if (hasMatch) {
-          return <React.Fragment>{props.children}</React.Fragment>; // show this child (a group)
-        }
-      }}
-    </SearchContext.Consumer>
   );
 };
 
---
+++ src/FilterForSearch.tsx
@@ -0,0 +1,58 @@
+import React, { useMemo, useState, useEffect, ReactElement, ReactNode } from 'react';
+import Children from 'react-children-utilities';
+export const SearchContext = React.createContext({
+  searchString: '' as string | null,
+  searchRegEx: null as RegExp | null,
+  setSearchString: (searchString: string | null) => {},
+});
+
+// If Search is empty, pass through.
+// Else, pass through so long as the given label or kids have the search term
+export const FilterForSearchText: React.FunctionComponent<{
+  label: string;
+  description?: string | React.ReactNode;
+  kids: React.ReactNode;
+}> = (props) => {
+  // enhance: would the "react-children-utilities/Children" utilities simplify this?
+  return (
+    <SearchContext.Consumer>
+      {({ searchRegEx }) => {
+        if (!searchRegEx) return <React.Fragment>{props.children}</React.Fragment>;
+        const hasMatch =
+          /* TODO: currently, if we match on a group's label or description,
+           but then don't match on a subgroup,
+          well we just get the label and that's confusing. */
+          searchRegEx.test(props.label) ||
+          (props.description && searchRegEx.test(Children.onlyText(props.description))) ||
+          // check the children (rows of the group)
+          React.Children.toArray(props.kids).some((c) => {
+            const componentWithLabelProp = (c as any).props?.label;
+            if (componentWithLabelProp && searchRegEx.test(componentWithLabelProp))
+              // this grandchild has the search string, directly
+              return true;
+            const componentWithSearchTermsProp = (c as any).props?.searchTerms;
+            if (
+              componentWithSearchTermsProp &&
+              searchRegEx.test(componentWithSearchTermsProp)
+            )
+              return true;
+            // Is this child a subpage? Then it has children we can check.
+            const childrenOfSubPage = (c as any).props?.children;
+            if (childrenOfSubPage) {
+              // among these children...
+              return React.Children.toArray(childrenOfSubPage).some((c: any) => {
+                // ... is there a child (a row of a sub page) that has a label that matches?
+                if (c?.props?.label) {
+                  // check the label in a great-grand-child (row of a sub page)
+                  return searchRegEx.test(c.props.label);
+                }
+              });
+            }
+          });
+        if (hasMatch) {
+          return <React.Fragment>{props.children}</React.Fragment>; // show this child (a group)
+        }
+      }}
+    </SearchContext.Consumer>
+  );
+};
