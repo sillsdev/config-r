9d2323095283563d3b6e5b4dfc71995774c975f3
Hatton <hattonjohn@gmail.com>
2022-03-12 10:01:48 -0700

    emit module files

---
+++ src/lib/index.ts
@@ -0,0 +1,1 @@
+export { ConfigrPane } from './ConfigrPane';
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,24 @@ export default defineConfig({
   esbuild: {
     jsxFactory: `jsx`,
     jsxInject: `import { jsx } from '@emotion/react'`,
+  },
+
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'configr',
+      fileName: (format) => `configr.${format}.js`,
+    },
+    rollupOptions: {
+      // don't bundle these with the library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+        },
+      },
+    },
   },
 });
