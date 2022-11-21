7f64e353036316e961f4aa944e26c73956e18695
John Thomson <john_thomson@sil.org>
2022-11-21 17:08:08 -0600

    Added (incomplete) story for book settings

--- src/stories/bloom.stories.tsx
+++ src/stories/bloom.stories.tsx
@@ -280,3 +280,162 @@ const BloomCollectionInner: React.FunctionComponent<{
     </div>
   );
 };
+
+
+export const BloomBook:  React.FunctionComponent = (props)  =>{
+  const [results, setResults] = useState("");
+  return  <div css={css`display:flex`}>
+    <BloomBookInner setValueOnRender={(currentValues)=> {
+      const pretty = JSON.stringify(currentValues, null, 4);
+      setResults(pretty);
+    }}/>
+    <div css={css`white-space:pre; margin-left: 20px;`}>{results}</div>
+  </div>;
+}
+
+// A very rough first draft based on the options I see in each publish tab
+// This is way more than we want in V1 of the Book options but I thought it was an interesting
+// experiment to see which ones are problems with our current repertoir.
+const initialBloomBookValues = {
+  appearance: {
+    cover: {
+      coverColor: "#ffcc00",
+    },
+    margins: {
+      marginTop: "44mm",
+      marginBottom: "44mm",
+      marginOuter: "44mm",
+      marginInner: "44mm",
+    },
+    spacing: {
+      verticalBlocks: "10mm"
+    }
+  },
+  pdfPrint: {
+    cmyk: false,
+    fullBleed: false
+
+  }, bloomPub: {
+    motion: false,
+    languages: [
+      {code: "fr", name: "French", include: true, complete: false}
+      ],
+    narrationLanguages: [
+      {code: "fr", name: "French", include: false, present: false}
+      ]
+  },
+  bloomLibrary: {
+    summary: "a book",
+    // Somehow the appropriate control needs to know whether it should be enabled.
+    // enabled: doesn't really belong here, as it's not data we want to persist.
+    accessible: false,
+    signLanguages: false,
+    languages: [
+      // complete, and the name, represent additional data that we need to make
+      // the UI look right, but they are changeable properties of the other data
+      // in the book, not part of saved settings. How should we handle that?
+      // Likewise name and present below.
+      {code: "fr", name: "French", include: true, complete: false}
+      ],
+    narrationLanguages: [
+      {code: "fr", name: "French", include: false, present: false}
+      ],
+    music: false
+    // sign languguage separately? or oe of languages?
+  },
+  epub: {
+    options: {
+      mode: "fixed",
+      // Want a way to say this should be disabled if book has none
+      // That isn't part of the saved options, so probably should be communicated to the
+      // tool another way. How?
+      imageDescriptionsOnPage: false,
+    },
+    metadata: {
+      author: "Joe",
+      summary: "a book", // should this be the same summary as in bloomLibrary?
+      ageRange: "5-8",
+      level: 3, // related to leveled reader level?
+      subjects: ["crime"],
+      accessabilityLevel: "Level AA Conformance",
+      accessibilityCertifiedBy: "me",
+      flashingHazard: undefined, // Todo: want this to mean "unknown" for tristate checkbox
+      motionHazard: false,
+      hasImageDescriptions: false, // related to whether imageDescriptionsOnPage is enabled?
+      hasSignLanguage: false
+    }
+  }
+};
+
+// This doesn't yet have Configr elements for all the options above. I wanted to focus on things that might be a problem.
+// One I don't know how to do at all with the current components is the list of checkboxes for which languages to include.
+const BloomBookInner: React.FunctionComponent<{
+  setValueOnRender?: (currentValues: any) => void; // just used to see the realtime value
+}> = (props) => {
+  const bloomThemeOverrides = {
+    palette: {
+      primary: {
+        main: '#1D94A4',
+      },
+    },
+  };
+  return (
+    <div css={css`
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+      `}>
+      <ConfigrPane
+        label="Book Settings"
+        initialValues={initialBloomBookValues}
+        themeOverrides={bloomThemeOverrides}
+        showSearch={true}
+        {...props}
+      >
+        <ConfigrGroup label="Appearance" level={1}>
+          <ConfigrSubgroup label= "Cover" path='appearance.cover'>
+            <ConfigrInput path={`appearance.cover.coverColor`} label="Cover Color" />
+          </ConfigrSubgroup>
+          <ConfigrSubgroup label= "Margins" path='appearance.margins'>
+            <ConfigrInput path={`appearance.margins.marginTop`}
+              // wants a way to say it can be mm, or in, or pt, or...
+              label="Top" />
+            <ConfigrInput path={`appearance.margins.marginBottom`} label="Bottom" />
+            <ConfigrInput path={`appearance.margins.marginOuter`} label="Outer" />
+            <ConfigrInput path={`appearance.margins.marginInner`} label="Inner" />
+          </ConfigrSubgroup>
+          <ConfigrSubgroup label= "Spacing" path='appearance.cover'>
+            <ConfigrInput path={`appearance.spacing.verticalBlocks`} label="Between Vertical Blocks" />
+          </ConfigrSubgroup>
+          
+        </ConfigrGroup>
+        <ConfigrGroup label="Bloom Library" level={1}>
+          <ConfigrInput path={`bloomLibrary.summary`} label="Summary"
+          // Wants a way to say to leave more space for a summary.
+           />
+        </ConfigrGroup>
+        <ConfigrGroup label="ePUB" level={1}>
+          <ConfigrSubgroup label= "Options" path='epub.options'>
+            <ConfigrSelect
+              path={'epub.options.mode'}
+              label="ePUB mode"
+              options={[
+                { label: 'Fixed - ePUB 3', value: 'fixed', description:"Ask ePUB reader to show pages exactly like you see them in Bloom" },
+                { label: 'Flowable', value: 'flowable',  description:"Allow ePUB readers to lay out images and text however they like"},
+              ]}
+            />
+            <ConfigrBoolean label="Image Descriptions on Page" path="epub.options.imageDescriptionsOnPage"/>
+          </ConfigrSubgroup>
+          <ConfigrSubgroup label= "Metadata" path='epub.metadata'>
+            <ConfigrInput path={`epub.metadata.author`} label="Author" />
+            <ConfigrBoolean label="Flashing Hazard" path="epub.options.flashingHazard"
+            // Want a way that this can be undefined meaning unknown
+            // May want to group these as "Hazards" but trying to avoid too many levels
+            />
+            <ConfigrBoolean label="Motion Simulation Hazard" path="epub.options.motionHazard"/>
+          </ConfigrSubgroup>
+          
+        </ConfigrGroup>
+      </ConfigrPane>
+    </div>);
+  }
