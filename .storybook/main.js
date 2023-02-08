cb0308101be64ae15f9ace0748ba1df44d213888
Hatton <hattonjohn@gmail.com>
2023-02-08 16:51:50 -0700

    upgrade vite  storybook stuff, using `npx sb@latest upgrade`

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,15 @@
 const react = require('@vitejs/plugin-react');
-
 module.exports = {
   async viteFinal(config) {
     // from https://github.com/storybookjs/builder-vite/issues/210
-    config.plugins = config.plugins.filter(
-      (plugin) => !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react')),
-    );
-
-    config.plugins.push(
-      react({
-        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
-        jsxImportSource: '@emotion/react',
-        babel: {
-          plugins: ['@emotion/babel-plugin'],
-        },
-      }),
-    );
-
+    config.plugins = config.plugins.filter(plugin => !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react')));
+    config.plugins.push(react({
+      exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin']
+      }
+    }));
     console.log(config.plugins);
     return config;
   },
@@ -24,6 +17,6 @@ module.exports = {
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
   core: {
-    builder: 'storybook-builder-vite',
-  },
+    builder: '@storybook/builder-vite'
+  }
 };
