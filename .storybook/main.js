3bdc0d2a4c75bb69bc0dbb5b1983f56f5af9aee5
Hatton <hattonjohn@gmail.com>
2023-02-08 17:27:04 -0700

    prettier changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,14 +2,18 @@ const react = require('@vitejs/plugin-react');
 module.exports = {
   async viteFinal(config) {
     // from https://github.com/storybookjs/builder-vite/issues/210
-    config.plugins = config.plugins.filter(plugin => !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react')));
-    config.plugins.push(react({
-      exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin']
-      }
-    }));
+    config.plugins = config.plugins.filter(
+      (plugin) => !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react')),
+    );
+    config.plugins.push(
+      react({
+        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      }),
+    );
     console.log(config.plugins);
     return config;
   },
@@ -17,6 +21,6 @@ module.exports = {
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
   core: {
-    builder: '@storybook/builder-vite'
-  }
+    builder: '@storybook/builder-vite',
+  },
 };
