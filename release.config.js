0fc6902140e2aa2060f75bab0ed4015996b6373d
Hatton <hattonjohn@gmail.com>
2023-02-10 10:26:33 -0700

    Simplify semantic-release config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  pkgRoot: 'dist',
   branches: ['release', { name: 'master', channel: 'alpha', prerelease: 'alpha' }],
-  repositoryUrl: 'https://github.com/sillsdev/config-r.git',
 };
