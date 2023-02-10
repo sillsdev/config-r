960150cc3400935902795b459f6c1a7aae1f5e6f
Hatton <hattonjohn@gmail.com>
2023-02-10 10:04:25 -0700

    Back off of semantic-release plugins for now

--- release.config.js
+++ release.config.js
@@ -2,19 +2,4 @@ module.exports = {
   pkgRoot: 'dist',
   branches: ['release', { name: 'master', channel: 'alpha', prerelease: 'alpha' }],
   repositoryUrl: 'https://github.com/sillsdev/config-r.git',
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
-    '@semantic-release/npm',
-    [
-      '@semantic-release/github',
-      {
-        assets: {
-          path: './CHANGELOG.md',
-          label: 'Changelog for this release.',
-        },
-      },
-    ],
-  ],
 };
