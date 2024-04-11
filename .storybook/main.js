14649823c78af489ba8d7f6a5577e31f6ef45797
Hatton <hattonjohn@gmail.com>
2024-04-11 16:01:47 -0600

    Upgrade storybook so that react devtools work

--- .storybook/main.js
+++ .storybook/main.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/react',
+  framework: '@storybook/react-vite',
   core: {
     builder: '@storybook/builder-vite',
   },
