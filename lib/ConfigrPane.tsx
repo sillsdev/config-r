e4048ce96d3c55cf9732d5f6d20f34d57d23b852
Steve McConnel <stephen_mcconnel@sil.org>
2026-04-14 12:24:25 -0600

    Retain height of parent for ConfigrPane to allow scrolling (BL-16159)

--- lib/ConfigrPane.tsx
+++ lib/ConfigrPane.tsx
@@ -122,6 +122,7 @@ export const ConfigrPane: React.FunctionComponent<
           display: flex;
           flex-direction: row;
           flex: 1;
+          height: 100%; // retain height of parent for possibly scrollable content (BL-16149)
         `}
       >
         <div
